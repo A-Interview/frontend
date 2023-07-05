b057f0a76159bc94718cd20d5db0667d45f13fa9
hsjni0110 <hansukjinid@gmail.com>
2023-07-05 14:49:18 +0900

    feat:메인 페이지 프레임 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
 import React from "react";
-import "./App.css";
+import MainPage from "./pages/MainPage";
 
 function App(): JSX.Element {
-  const hi = 0;
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx {hi}</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MainPage />
     </div>
   );
 }
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainPage = (): JSX.Element => {
+  return <div></div>;
+};
+
+export default MainPage;
