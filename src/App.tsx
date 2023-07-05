cc8d13456b58fc852a3bf387e520db44bdd987b7
hsjni0110 <hansukjinid@gmail.com>
2023-07-05 12:18:09 +0900

    init:eslint 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+function App (): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
@@ -19,7 +19,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
