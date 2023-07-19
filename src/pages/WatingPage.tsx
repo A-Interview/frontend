ecee7f3c42b4cf3b54523e213aa11d65e512bb5b
miyoung12 <miseja3@sungkyul.ac.kr>
2023-07-19 22:42:35 +0900

    진짜 마지막 수정

--- src/pages/WatingPage.tsx
+++ src/pages/WatingPage.tsx
@@ -45,7 +45,6 @@ const RightContainer = styled.div`
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
   filter: blur(1.5px);
   background-size: cover;
-  z-index: 1;
 `;
 const RequestText = styled.p`
   color: #fff;
