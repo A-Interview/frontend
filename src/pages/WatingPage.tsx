5145833ab9800ba45811ce86fc044d7737031de2
박준서 <jswing5267@naver.com>
2023-07-27 13:57:19 +0900

    메인 업데이트

--- src/pages/WatingPage.tsx
+++ src/pages/WatingPage.tsx
@@ -241,9 +241,10 @@ const WatingPage = (): JSX.Element => {
   // 현재 Form의 갯수가 4개를 넘어가는 지 체크하는 함수
   const checkFormNumber = (): void => {
     const accessToken: string | null = sessionStorage.getItem("access_token");
-    if (accessToken != null) {
+    const userId: string | null = sessionStorage.getItem("user_id");
+    if (accessToken != null && userId != null) {
       axios
-        .get("/api/forms", {
+        .get(`${process.env.REACT_APP_API_URL_FORM}${userId}/`, {
           headers: {
             Authorization: `Bearer ${accessToken}`,
           },
