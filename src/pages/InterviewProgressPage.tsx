57a9a0cddd8b4953ff3c4c4eca8243d703d619eb
hsjni0110 <hansukjinid@gmail.com>
2023-07-31 17:40:25 +0900

    feat: develop version 추가

--- src/pages/InterviewProgressPage.tsx
+++ src/pages/InterviewProgressPage.tsx
@@ -214,8 +214,6 @@ const CameraButton = styled.button`
   top: 1.9rem;
 `;
 
-const HOST = process.env.REACT_APP_HOST;
-
 const InterviewProgressPage = (): JSX.Element => {
   // 페이지 네비게이션
   const navigate = useNavigate();
@@ -325,10 +323,9 @@ const InterviewProgressPage = (): JSX.Element => {
   // last_topic_answer 토큰 변수
   const [lastTopicAnswer, setLastTopicAnswer] = useState("");
 
-
   // 소켓 연결 함수, 메세지 처리 기능
   const connectWebSocket = (): void => {
-    const ws = new WebSocket(`wss://${HOST as string}/ws/interview/`);
+    const ws = new WebSocket(`ws://localhost:8000/ws/interview/`);
 
     ws.onopen = () => {
       console.log("WebSocket connected");
